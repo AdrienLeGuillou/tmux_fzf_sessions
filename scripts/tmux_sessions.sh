dcb3f6b6e70a1419c7b15ea0b239c492e247a9a5
Adrien Le Guillou <git@aleguillou.org>
2023-11-21 15:01:02 +0100

    Update tmux_sessions.sh
    make it work with spaces

--- scripts/tmux_sessions.sh
+++ scripts/tmux_sessions.sh
@@ -4,15 +4,15 @@
 main () {
     tmuxp_dir="$HOME/.config/tmuxp/"
     active_sessions=$(tmux list-sessions)
-    saved_sessions=$(ls $tmuxp_dir)
+    saved_sessions=$(ls "$tmuxp_dir")
     all_sessions="$active_sessions"$'\n'"$saved_sessions"
     all_sessions=$(echo "$all_sessions" | grep -o "^[^:\.]*" | sort -u)
     target_session=$(echo "$all_sessions" | fzf | grep -o "^[^:\.]*")
 
-    tmux has-session -t $target_session &>/dev/null || \
+    tmux has-session -t "$target_session" &>/dev/null || \
         tmuxp load "$tmuxp_dir$target_session.yaml" -d
 
-    tmux switch-client -t $target_session
+    tmux switch-client -t "$target_session"
 }
 
 main
